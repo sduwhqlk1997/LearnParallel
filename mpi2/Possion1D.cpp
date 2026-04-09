24502350d2058b1d6fc45477856a0f011dfb13c5
qlk <445478113@qq.com>
2026-04-09 19:55:18 +0800

    finish Possion1D

--- mpi2/Possion1D.cpp
+++ mpi2/Possion1D.cpp
@@ -5,7 +5,7 @@ int rank,size;
 int main(int argc, char* argv[]){
     int n = 18;
     int localn;
-    int maxt = 10000;
+    int maxt = 100;
     double length = 1.0;
     double dt = 0.001;
     double dx = length/n;
@@ -22,6 +22,17 @@ int main(int argc, char* argv[]){
     localn = n/size;
     std::vector<double> T(localn+2);
     std::vector<double> newT(localn+2);
+
+    for(int i=0;i<maxt;++i){
+        MPI_Sendrecv(&T[1],1,MPI_DOUBLE,left,0,&T[localn+1],1,MPI_DOUBLE,right,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        MPI_Sendrecv(&T[localn],1,MPI_DOUBLE,right,1,&T[0],1,MPI_DOUBLE,left,1,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        if(rank==0) T[0]=1.0;
+        if(rank==size-1) T[localn+1]=0.0;
+        for(int j=1;j<=localn;++j){
+            newT[j] = T[j]+a*(T[j-1]-2*T[j]+T[j+1]);
+        }
+        T.swap(newT);
+    }
     MPI_Finalize();
     return 0;
 }
